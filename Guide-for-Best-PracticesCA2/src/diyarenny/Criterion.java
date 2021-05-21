c67cc04e119200497bbd0672c3b76c239c2c02f9
diyarenny <diya.renny@mydit.ie>
2021-05-21 03:56:30 +0100

    Criterion object class defined

--- Guide-for-Best-PracticesCA2/src/diyarenny/Criterion.java
+++ Guide-for-Best-PracticesCA2/src/diyarenny/Criterion.java
@@ -2,4 +2,23 @@ package diyarenny;
 
 public class Criterion {
 
+	String criterionName;
+
+	public Criterion() {
+
+	}
+
+	public Criterion(String criterionName) {
+		this.criterionName = criterionName;
+
+	}
+
+	public String getCriterionName() {
+		return criterionName;
+	}
+
+	public void setCriterionName(String criterionName) {
+		this.criterionName = criterionName;
+	}
+
 }
