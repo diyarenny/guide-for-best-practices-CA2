66bac84d39bdd39e9520193e635cdd53340481da
diyarenny <diya.renny@mydit.ie>
2021-05-21 04:00:35 +0100

    Student.java defined and variables set

--- Guide-for-Best-PracticesCA2/src/diyarenny/Student.java
+++ Guide-for-Best-PracticesCA2/src/diyarenny/Student.java
@@ -2,4 +2,23 @@ package diyarenny;
 
 public class Student {
 
+	String studentName;
+
+	public Student() {
+		super();
+	}
+
+	public Student(String studentName) {
+		super();
+		this.studentName = studentName;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
 }
