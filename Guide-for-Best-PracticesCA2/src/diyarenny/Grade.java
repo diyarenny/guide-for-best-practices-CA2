16b425064d2f19b6cb4f45c9d94142681488d18b
diyarenny <diya.renny@mydit.ie>
2021-05-21 03:57:49 +0100

    Grade class created

--- Guide-for-Best-PracticesCA2/src/diyarenny/Grade.java
+++ Guide-for-Best-PracticesCA2/src/diyarenny/Grade.java
@@ -2,4 +2,23 @@ package diyarenny;
 
 public class Grade {
 
+	int score;
+
+	public Grade() {
+
+	}
+
+	public Grade(int score) {
+		super();
+		this.score = score;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
 }
